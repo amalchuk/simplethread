d73bb37cad396a315364bb65f0ab8871a7bb7944
Andrew Malchuk <andrew.malchuk@yandex.ru>
2021-01-12 18:47:10 +0400

    Tue, 12 Jan 2021 14:47:09 +0000

--- simplethread/protocols.py
+++ simplethread/protocols.py
@@ -9,9 +9,9 @@ __all__: List[str] = ["LockType"]
 @runtime_checkable
 class LockType(Protocol):
     @abstractmethod
-    def acquire(self, blocking: bool, timeout: Optional[float]) -> bool:
+    def acquire(self, blocking: bool, timeout: Optional[float]) -> bool:  # pragma: no cover
         raise NotImplementedError
 
     @abstractmethod
-    def release(self) -> None:
+    def release(self) -> None:  # pragma: no cover
         raise NotImplementedError
