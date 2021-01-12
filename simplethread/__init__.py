88bd316d4b82d753d315326f4b750d359441eed2
Andrew Malchuk <andrew.malchuk@yandex.ru>
2021-01-12 11:26:57 +0400

    Tue, 12 Jan 2021 07:26:57 +0000

--- simplethread/__init__.py
+++ simplethread/__init__.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
 """
-Some useful utilities for Python's ``threading`` module.
+Some useful utilities for Python's ``threading`` and ``greenlet`` modules.
 """
 
+from typing import List
+
+from simplethread.decorators import asynchronous
 from simplethread.decorators import synchronized
 from simplethread.decorators import threaded
 
-__all__ = ("synchronized", "threaded")
+__all__: List[str] = ["asynchronous", "synchronized", "threaded"]
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,20 +1,56 @@
 # -*- coding: utf-8 -*-
 
-from simplethread.decorators import synchronized
-from simplethread.decorators import threaded
+from threading import Event
+from time import sleep
+
+from simplethread import asynchronous
+from simplethread import synchronized
+from simplethread import threaded
 
 
-def multiply(a: float, b: float) -> float:
+def _multiply(a: float, b: float) -> float:
+    """
+    Same as a × b.
+    """
     return a * b
 
 
+def test_asynchronous() -> None:
+    multiply = asynchronous(_multiply)
+    assert multiply(0, 2) == 0
+    assert multiply(2, 4) == 8
+    assert multiply(4, 8) == 32
+
+
 def test_synchronized() -> None:
-    initial_value: int = 5
-    decorated = synchronized(multiply)
-    assert decorated(initial_value, 3) == 15
+    multiply = synchronized(_multiply)
+    assert multiply(0, 2) == 0
+    assert multiply(2, 4) == 8
+    assert multiply(4, 8) == 32
 
 
 def test_threaded() -> None:
     initial_value: int = 3
-    decorated = threaded(multiply)
-    assert decorated(initial_value, 5).result() == 15
+    event: Event = Event()
+
+    @threaded
+    def slow_method(a: float) -> None:
+        nonlocal initial_value, event
+        sleep(0.25)
+        initial_value = _multiply(a, initial_value)
+        return event.set()
+
+    # Start the separate thread to multiply initial_value by 5:
+    slow_method(5)
+    # Wait until the thread is finished:
+    event.wait()
+    assert initial_value == 15
+
+    # Repeat again:
+    event.clear()
+
+    # Start the separate thread to multiply initial_value by 2:
+    slow_method(2)
+    # Wait until the thread is finished:
+    event.wait()
+    assert initial_value == 30
